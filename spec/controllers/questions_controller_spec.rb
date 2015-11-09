a9bb72e84e9d953dc8cdac308c01cda6863597a5
s-anches <maks.portnyagin@gmail.com>
2015-11-10 01:11:56 +0700

    Add green test to index, new, create, show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe "GET #index" do
-    before do
-      get :index
-    end
+    let(:questions) { create_list(:question, 2) }
+
+    before { get :index }
 
     it "show all questions" do
-      @questions = create_list(:question, 2)
-      expect(assigns(:questions)).to match_array(@questions)
+      expect(assigns(:questions)).to match_array(questions)
     end
 
     it "render index view" do
@@ -17,8 +18,53 @@ RSpec.describe QuestionsController, type: :controller do
 
   end
 
-  describe "GET #new " do
-    
+  describe "GET #show" do
+
+    before { get :show, id: question }
+
+    it "show question" do
+      expect(assigns(:question)).to eq question
+    end
+
+    it "render show view" do
+      expect(response).to render_template :show
+    end
+  end
+  
+  describe "GET #new" do
+    before { get :new }
+
+    it "assigns a new Question to Question" do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it "render new view" do
+      expect(response).to render_template :new
+    end
   end
 
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "save the new question" do
+        expect { post :create, question: attributes_for(:question) }.to change(Question, :count).by(1)
+      end
+
+      it "redirect to show this question" do
+        post :create, question: attributes_for(:question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context "with invalid attributes" do
+      it "don't save the new question" do
+        expect { post :create, question: attributes_for(:wrong_question) }.to_not change(Question, :count)
+      end
+
+      it "redirect to create" do
+        post :create, question: attributes_for(:wrong_question)
+        expect(response).to render_template :new
+      end
+    end
+  end
+  
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -4,4 +4,9 @@ FactoryGirl.define do
     body "MyText"
   end
 
+  factory :wrong_question, class: "Question" do
+    title nil
+    body nil
+  end
+
 end
