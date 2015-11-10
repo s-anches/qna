103a1cd26b4b84e3de2da6592e861bf994437b27
s-anches <maks.portnyagin@gmail.com>
2015-11-10 21:46:28 +0700

    Add green #new and #create test

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+  let(:answer) { create(:answer) }
+  let(:wrong_answer) { create(:wrong_answer) }
+
+  describe "GET #new" do
+    before { get :new, question_id: question.id }
+
+    it "assigns a new Answer to Answer" do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it "render new view" do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "save the new answer to question" do
+        expect { post :create, question_id: question.id, answer: attributes_for(:answer) }.to change(Answer, :count).by(1)
+      end
+
+      it "redirect to show this question" do
+        post :create, question_id: question.id, answer: attributes_for(:answer)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context "with invalid attributes" do
+      it "don't save the new answer" do
+        expect { post :create, question_id: question.id, answer: attributes_for(:wrong_answer) }.to_not change(Answer, :count)
+      end
+
+      it "redirect to create" do
+        post :create, question_id: question.id, answer: attributes_for(:wrong_answer)
+        expect(response).to render_template :new
+      end
+    end
+  end
+end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,28 @@
 FactoryGirl.define do
   factory :answer do
-    body "MyText"
+    body "MyTest"
   end
 
+  factory :wrong_answer, class: "Answer" do
+    body nil
+  end
+  # factory :answer do
+  #   body "MyText"
+  #   questions
+
+  #   factory :question do
+  #     title "MyTitle"
+  #     body "MyBody"
+  #
+  #     factory :question_with_answers do
+  #       transient do
+  #         answer_count 5
+  #       end
+  #
+  #       after(:create) do |question, evaluator|
+  #         create_list(:answer, evaluator.answer_count, questio: questio)
+  #       end
+  #     end
+  #   end
+  # end
 end
