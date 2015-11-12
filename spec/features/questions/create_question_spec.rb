486ede42abd58c2eb159c6a757e12e643f2cc0fe
s-anches <maks.portnyagin@gmail.com>
2015-11-12 17:51:37 +0700

    Change user create question test with auth

--- spec/features/questions/create_question_spec.rb
+++ spec/features/questions/create_question_spec.rb
@@ -1,21 +1,44 @@
 require 'rails_helper'
 
 feature 'User can create new question', %q{
-  In order to be able to ask question
-  User go to new question page, fill need field and click save
-  User redirect to new question page or saw error
+  In order to get answer from community
+  As an authenticated user
+  I want to be able to ask questions
 } do
 
-  scenario 'User try to create new question with valid parameters' do
-    save_new_question("Title", "Body")
+  scenario 'Authenticated user try to create question with valid attributes' do
+    User.create!(email: 'user@example.com', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    save_new_question("First question", "This is first question")
 
     expect(page).to have_content "New question successfully created"
+    expect(page).to have_content "First question"
+    expect(page).to have_content "This is first question"
   end
 
-  scenario 'User try to create new question with invalid parameters' do
+  scenario 'Authenticated user try to create question with invalid attributes' do
+    User.create!(email: 'user@example.com', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
     save_new_question
 
     expect(page).to have_content "Some errors occured"
+  end
+
+  scenario 'Non-authenticated user try to create question' do
+    visit new_question_path
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    expect(current_path).to eq new_user_session_path
   end
 
 end
--- spec/support/acceptance_helpers.rb
+++ spec/support/acceptance_helpers.rb
@@ -1,15 +1,15 @@
 module AcceptanceHelper
   def save_new_question(title = nil, body = nil)
     visit new_question_path
-    fill_in "Title", with: title
-    fill_in "Body", with: body
+    fill_in 'Title', with: title
+    fill_in 'Body', with: body
     click_on 'Create'
   end
 
   def save_new_answer(question, body = nil)
     visit new_question_answer_path(question)
 
-    fill_in "Body", with: body
-    click_on "Create"
+    fill_in 'Body', with: body
+    click_on 'Create'
   end
 end
