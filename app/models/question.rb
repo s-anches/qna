6a0b90444f263563d656444a70cec505f24d3cba
s-anches <maks.portnyagin@gmail.com>
2015-11-30 00:13:31 +0700

    Add vote model and polymorphic association

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,10 +2,12 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers, dependent: :destroy
   has_many :attachments, as: :attachable, dependent: :destroy
+  has_many :votes, as: :votable, dependent: :destroy
 
   accepts_nested_attributes_for :attachments,
           reject_if: proc{ |param| param[:file].blank? },
           allow_destroy: true
+  accepts_nested_attributes_for :votes
 
   validates :user_id, :title, :body, presence: true
 end
